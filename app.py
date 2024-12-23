d457b7cc67a0dcda420f14c454a3e7fc310c1dc1
108krishna108 <162606834+108krishna108@users.noreply.github.com>
2024-12-23 05:44:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure all tables are created
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
